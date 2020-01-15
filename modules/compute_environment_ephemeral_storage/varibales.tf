701fa28f74bf536f6d71886c8b93c6a003ced947
Thomas Maschler <tmaschler@wri.org>
2020-01-15 16:33:12 -0500

    fix variable file

--- modules/compute_environment_ephemeral_storage/varibales.tf
+++ modules/compute_environment_ephemeral_storage/varibales.tf
@@ -1,8 +1,28 @@
-variable "project" { type = string }
-variable "key_pair" { type = string }
-variable "subnets" { type = list(string) }
-variable "tags" { type = map(string) }
-variable "security_group_ids" { type = list(string) }
-variable "iam_policy_arn" { type = list(string) }
-variable "suffix" { type = string
-                    default = ""}
+variable "project" {
+  type = string
+}
+
+variable "key_pair" {
+  type = string
+}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "security_group_ids" {
+  type = list(string)
+}
+
+variable "iam_policy_arn" {
+  type = list(string)
+}
+
+variable "suffix" {
+  type = string
+  default = ""
+}
