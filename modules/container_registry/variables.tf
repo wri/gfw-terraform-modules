4a484bc7c04b87dcef6cb8a43de52e9d48d67bcc
Thomas Maschler <tmaschler@wri.org>
2020-01-18 14:37:24 -0500

    remove quotes from string type declaration

--- modules/container_registry/variables.tf
+++ modules/container_registry/variables.tf
@@ -1,34 +1,34 @@
 variable "image_name" {
   description = "Name of Docker image"
-  type        = "string"
+  type        = string
 }
 
 variable "root_dir" {
   description = "Root dir used in Dockerfile"
-  type        = "string"
+  type        = string
   default     = "."
 }
 
 variable "docker_path"{
   description = "Path to Docker image source, relative to root dir"
-  type        = "string"
+  type        = string
   default     = "."
 }
 
 variable "tag" {
   description = "Tag to use for deployed Docker image"
-  type        = "string"
+  type        = string
   default     = "latest"
 }
 
 variable "hash_script" {
   description = "Path to script to generate hash of source contents"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "push_script" {
   description = "Path to script to build and push Docker image"
-  type        = "string"
+  type        = string
   default     = ""
 }
