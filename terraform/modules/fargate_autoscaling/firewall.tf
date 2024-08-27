b85c08d54efca129e1a760cc45dffa197b3d23b3
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-08-27 00:44:57 -0400

    Simplify, hopefully not break firewall

--- terraform/modules/fargate_autoscaling/firewall.tf
+++ terraform/modules/fargate_autoscaling/firewall.tf
@@ -1,176 +1,97 @@
-## ALB Security group
-## This is the group you need to edit if you want to restrict access to your application
-#resource "aws_security_group" "lb" {
-#  count       = var.load_balancer_security_group == "" ? 1 : 0
-#  name        = substr("${var.project}-ecs-alb${var.name_suffix}", 0, 64)
-#  description = "Controls access to the ALB"
-#  vpc_id      = var.vpc_id
-#
-#  tags = merge(
-#    {
-#      Name = substr("${var.project}-ecs-alb${var.name_suffix}", 0, 64)
-#    },
-#    var.tags
-#  )
-#}
-#
-## When using SSL certificate open port 80 for ingress, otherwise use specific port
-#resource "aws_vpc_security_group_ingress_rule" "ingress_http" {
-#  count             = var.load_balancer_security_group == "" ? 1 : 0
-#  security_group_id = aws_security_group.lb.id
-#  protocol          = "tcp"
-#  from_port         = var.acm_certificate_arn == null ? 80 : var.listener_port
-#  to_port           = var.acm_certificate_arn == null ? 80 : var.listener_port
-#  cidr_blocks       = ["0.0.0.0/0"]
-#}
-#
-#resource "aws_vpc_security_group_ingress_rule" "ingress_https" {
-#  count             = var.load_balancer_security_group == "" ? 1 : 0
-#  security_group_id = aws_security_group.lb.id
-#  protocol          = "tcp"
-#  from_port         = 443
-#  to_port           = 443
-#  cidr_blocks       = ["0.0.0.0/0"]
-#}
-#
-## Open container port for egress and link with ECS Task security group
-#resource "aws_vpc_security_group_egress_rule" "lb_task_egress" {
-#  security_group_id        = length(aws_security_group.lb) == 1 ? aws_security_group.lb[0].id : var.load_balancer_security_group
-#  from_port                = var.container_port
-#  to_port                  = var.container_port
-#  protocol                 = "tcp"
-#  source_security_group_id = aws_security_group.ecs_tasks.id
-#}
-#
-#
-## Traffic to the ECS Cluster should only come from the ALB
-## Tasks should be able to communicate with any external resource
-#resource "aws_security_group" "ecs_tasks" {
-#  name        = substr("${var.project}-ecs-tasks${var.name_suffix}", 0, 64)
-#  description = "Allow inbound access from the ALB only"
-#  vpc_id      = var.vpc_id
-#
-#  tags = merge(
-#    {
-#      Name = substr("${var.project}-ecs-tasks${var.name_suffix}", 0, 64)
-#    },
-#    var.tags
-#  )
-#}
-#
-##resource "aws_vpc_security_group_ingress_rule" "lb" {
-##  protocol          = "tcp"
-##  from_port         = var.container_port
-##  to_port           = var.container_port
-##  security_groups = length(aws_security_group.lb) > 0 ? [aws_security_group.lb[0].id] : [var.load_balancer_security_group]
-##}
-#
-#resource "aws_vpc_security_group_egress_rule" "lb_egress" {
-#  protocol    = "-1"
-#  from_port   = 0
-#  to_port     = 0
-#  cidr_blocks = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.ecs_tasks.id
-#}
-#
-#resource "aws_vpc_security_group_ingress_rule" "lb_ingress" {
-#  count             = var.load_balancer_security_group == "" ? 0 : 1
-#  from_port         = var.container_port
-#  to_port           = var.container_port
-#  protocol          = "tcp"
-#  cidr_blocks       = ["0.0.0.0/0"]
-#  security_group_id = var.load_balancer_security_group
-#}
-#
-#
-## Allow Batch instances to reach the API
-#resource "aws_security_group" "batch_instances" {
-#  name        = substr("${var.project}-batch-instances${var.name_suffix}", 0, 64)
-#  description = "Security group for AWS Batch instances"
-#  vpc_id      = var.vpc_id
-#
-#  tags = merge(
-#    {
-#      Name = substr("${var.project}-batch-instances${var.name_suffix}", 0, 64)
-#    },
-#    var.tags
-#  )
-#}
-#
-#resource "aws_vpc_security_group_ingress_rule" "ecs_tasks_ingress_from_batch" {
-#  security_group_id        = aws_security_group.ecs_tasks.id
-#  from_port                = var.listener_port
-#  to_port                  = var.listener_port
-#  protocol                 = "tcp"
-#  source_security_group_id = aws_security_group.batch_instances.id
-#}
-#
-#resource "aws_vpc_security_group_egress_rule" "batch_to_ecs_egress" {
-#  security_group_id        = aws_security_group.batch_instances.id
-#  from_port                = var.listener_port
-#  to_port                  = var.listener_port
-#  protocol                 = "tcp"
-#  source_security_group_id = aws_security_group.ecs_tasks.id
-#}
-
+# Load Balancer Security Group
 resource "aws_security_group" "lb" {
-  count       = var.load_balancer_security_group == "" ? 1 : 0
   name        = substr("${var.project}-ecs-alb${var.name_suffix}", 0, 64)
   description = "Controls access to the ALB"
   vpc_id      = var.vpc_id
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = var.acm_certificate_arn == null ? 80 : var.listener_port
-    to_port     = var.acm_certificate_arn == null ? 80 : var.listener_port
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = merge(
-    {
-      Name = substr("${var.project}-ecs-alb${var.name_suffix}", 0, 64)
-    },
+    { Name = substr("${var.project}-ecs-alb${var.name_suffix}", 0, 64) },
     var.tags
   )
 }
 
+# Load Balancer Ingress Rules
+resource "aws_security_group_ingress_rule" "lb_ingress_http" {
+  security_group_id = aws_security_group.lb.id
+  protocol          = "tcp"
+  from_port         = var.acm_certificate_arn == null ? 80 : var.listener_port
+  to_port           = var.acm_certificate_arn == null ? 80 : var.listener_port
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_ingress_rule" "lb_ingress_https" {
+  count             = var.acm_certificate_arn == null ? 0 : 1
+  security_group_id = aws_security_group.lb.id
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Load Balancer Egress to ECS Tasks
+resource "aws_security_group_egress_rule" "lb_to_ecs_egress" {
+  security_group_id        = aws_security_group.lb.id
+  protocol                 = "tcp"
+  from_port                = var.listener_port
+  to_port                  = var.listener_port
+  source_security_group_id = aws_security_group.ecs_tasks.id
+}
+
+# ECS Tasks Security Group
 resource "aws_security_group" "ecs_tasks" {
   name        = substr("${var.project}-ecs-tasks${var.name_suffix}", 0, 64)
-  description = "Allow inbound access from the ALB only"
+  description = "Allow inbound access from the ALB and Batch"
   vpc_id      = var.vpc_id
 
-  ingress {
-    protocol        = "tcp"
-    from_port       = var.container_port
-    to_port         = var.container_port
-    security_groups = length(aws_security_group.lb) > 0 ? [aws_security_group.lb[0].id] : [var.load_balancer_security_group]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = merge(
-    {
-      Name = substr("${var.project}-ecs-tasks${var.name_suffix}", 0, 64)
-    },
+    { Name = substr("${var.project}-ecs-tasks${var.name_suffix}", 0, 64) },
     var.tags
   )
+}
+
+# ECS Tasks Ingress Rules
+resource "aws_security_group_ingress_rule" "ecs_tasks_ingress_from_lb" {
+  security_group_id        = aws_security_group.ecs_tasks.id
+  protocol                 = "tcp"
+  from_port                = var.listener_port
+  to_port                  = var.listener_port
+  source_security_group_id = aws_security_group.lb.id
+}
+
+resource "aws_security_group_ingress_rule" "ecs_tasks_ingress_from_batch" {
+  security_group_id        = aws_security_group.ecs_tasks.id
+  protocol                 = "tcp"
+  from_port                = var.listener_port
+  to_port                  = var.listener_port
+  source_security_group_id = aws_security_group.batch_instances.id
+}
+
+# ECS Tasks Egress Rule
+resource "aws_security_group_egress_rule" "ecs_tasks_egress" {
+  security_group_id = aws_security_group.ecs_tasks.id
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Batch Instances Security Group
+resource "aws_security_group" "batch_instances" {
+  name        = substr("${var.project}-batch-instances${var.name_suffix}", 0, 64)
+  description = "Security group for AWS Batch instances"
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    { Name = substr("${var.project}-batch-instances${var.name_suffix}", 0, 64) },
+    var.tags
+  )
+}
+
+# Batch Instances Egress to ECS Tasks
+resource "aws_security_group_egress_rule" "batch_to_ecs_egress" {
+  security_group_id        = aws_security_group.batch_instances.id
+  protocol                 = "tcp"
+  from_port                = var.listener_port
+  to_port                  = var.listener_port
+  source_security_group_id = aws_security_group.ecs_tasks.id
 }
